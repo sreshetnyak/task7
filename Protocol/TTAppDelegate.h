1c51bb7ecb098795fc631c6463f6049f36bcfd84
sreshetnyak <reshetniak.sergey@gmail.com>
2014-01-27 21:23:00 +0200

    Initial commit from local git

---
+++ Protocol/TTAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  TTAppDelegate.h
+//  Protocol
+//
+//  Created by sergey on 1/27/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TTViewController;
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TTViewController *viewController;
+
+@end
+
---
+++ Protocol/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  Protocol
+//
+//  Created by sergey on 1/27/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
